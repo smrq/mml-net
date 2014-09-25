8251a890176a683752b15f6876b8a9b357746ff9
Greg Smith <smr.kyu@gmail.com>
2014-09-25 18:51:58 -0500

    Heroku config

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -4,18 +4,5 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 module.exports = gulp.task('default', function () {
-  if (release) {
-    runSequence(
-      'clean',
-      ['index', 'styles', 'images', 'assets', 'templates', 'lint'],
-      'browserify',
-      ['minify', 'serve']
-    );
-  } else {
-    runSequence(
-      'clean',
-      ['index', 'styles', 'images', 'assets', 'templates', 'lint'],
-      ['watchify', 'watch', 'serve']
-    );
-  }
+  runSequence('build', 'serve');
 });
